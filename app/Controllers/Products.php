19d9e91ae8d1af161a71ae5c715ba777bc9e0eed
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-11-08 05:53:19 +0700

    wrapping list product response

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -1,5 +1,7 @@
-<?php namespace App\Controllers;
- 
+<?php
+
+namespace App\Controllers;
+
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
 use App\Models\ProductModel;
@@ -8,9 +10,22 @@ class Products extends ResourceController
 {
     use ResponseTrait;
     // Get All Product
-    public function index(){
+    public function index()
+    {
         $model = new ProductModel();
         $data = $model->findAll();
-        return $this->respond($data,200);
+        return $this->respond(['data' => $data], 200);
+    }
+
+    // Get Specific Product
+    public function show($id = null)
+    {
+        $model = new ProductModel();
+        $data = $model->getWhere(['id' => $id])->getResult();
+        if ($data) {
+            return $this->respond($data);
+        } else {
+            return $this->failNotFound('No Data Found with id ' . $id);
+        }
     }
 }
